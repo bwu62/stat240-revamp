3adfd3508c60214adcf844840a5c490d4cadd889
Bi Cheng Wu <bwu62@wisc.edu>
2024-06-27 10:24:40 -0500

    change script to copy demo.html based on modified time

--- _after.R
+++ _after.R
@@ -2,7 +2,7 @@
 if(
   file.exists("demo.html") && (
     !file.exists("docs/demo.html") || 
-    tools::md5sum("demo.html") != 
-    tools::md5sum("docs/demo.html")
+    file.info("demo.html")$mtime >=
+    file.info("docs/demo.html")$mtime
   )
 ) file.copy("demo.html","docs/",overwrite=T)
