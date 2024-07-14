2889a9194726ac5301ca1d8202a0f1c8ab4ad7c8
Bi Cheng Wu <bwu62@wisc.edu>
2024-07-14 13:04:02 -0500

    testing data/ index generation workflow

--- _after.R
+++ _after.R
@@ -1,13 +1,15 @@
 # copy if inexistent or changed
-copy.file = function(file){
+copy.file = function(file,dest=NULL){
+  if(is.null(dest)){
+    dest=paste0("docs/",file)
+  }
   if(
     file.exists(file) && (
       !file.exists(paste0("docs/",file)) || 
       file.info(file)$mtime >=
       file.info(paste0("docs/",file))$mtime
     )
-  ) file.copy(file,paste0("docs/",file),
-              overwrite=T)
+  ) file.copy(file,dest,overwrite=T)
 }
 
 # list of extra files/dirs to copy
@@ -32,3 +34,20 @@ for(path in copy.list){
   } else warning("Path wrong in copy list!")
 }
 
+# move data index around
+copy.file("docs/data-index.html",
+          "docs/data/index.html")
+copy.file("docs/data/index.html",
+          "data/index.html")
+
+# fix links in data index
+if(file.exists("docs/data/index.html")){
+  xfun::gsub_file("docs/data/index.html",
+                  '="libs',
+                  '="../libs')
+}
+if(file.exists("data/index.html")){
+  xfun::gsub_file("data/index.html",
+                  '="libs',
+                  '="../libs')
+}
