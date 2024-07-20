21b822f50e725c09a1d4690ab550e164164f5a71
Bi Cheng Wu <bwu62@wisc.edu>
2024-07-19 19:50:44 -0500

    import just pipe from magrittr, so it doesn't rely on my magrittr import in .Rprofile

--- _common.R
+++ _common.R
@@ -13,6 +13,9 @@ knitr::opts_chunk$set(
 # library(rvest)
 # library(downlit)
 
+# import just the pipe
+library(magrittr, include.only = c("%>%"))
+
 # detect which semester we're headed into
 # and fix an R-version to use
 
