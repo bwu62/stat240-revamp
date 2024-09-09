36d2a31b9420842513770b1b12b4282685dbfe17
Bi Cheng Wu <bwu62@wisc.edu>
2024-09-09 10:46:33 -0500

    tweak theming file

--- ggplot_theme_options.R
+++ ggplot_theme_options.R
@@ -2,12 +2,13 @@
 theme_set(theme_bw())
 
 # set better default colors for colorblind friendliness / better b/w printing
-options(ggplot2.discrete.fill = \(...) scale_fill_brewer(..., palette = "Set2"),
+options(ggplot2.discrete.fill   = \(...) scale_fill_brewer(...,  palette = "Set2"),
         ggplot2.discrete.colour = \(...) scale_color_brewer(..., palette = "Dark2"),
-        ggplot2.ordinal.colour = \(...) scale_color_brewer(..., palette = "Dark2"))
+        ggplot2.ordinal.fill    = \(...) scale_fill_brewer(...,  palette = "Set2"),
+        ggplot2.ordinal.colour  = \(...) scale_color_brewer(..., palette = "Dark2"))
 
 # make background transparent, looks better in alert boxes
-theme_update(plot.background = element_rect(fill = "transparent", color = NA),
-             panel.background = element_rect(fill = rgb(1, 1, 1, .75)),
+theme_update(plot.background   = element_rect(fill = "transparent", color = NA),
+             panel.background  = element_rect(fill = rgb(1, 1, 1, .75)),
              legend.background = element_rect(fill = "transparent", color = NA),
-             legend.key = element_rect(fill = "transparent", color = NA))
+             legend.key        = element_rect(fill = "transparent", color = NA))
